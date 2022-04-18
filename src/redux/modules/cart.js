0031b5f243d4afd18428067c91ca364330147892
cge1023 <ge931023@gmail.com>
2022-04-18 20:42:05 +0900

    merge 전 commit

--- src/redux/modules/cart.js
+++ src/redux/modules/cart.js
@@ -16,3 +16,38 @@ const initialState = {
     totalPrice: [],
 };
 
+const getCartDB = () => {
+    return async function (dispatch, getState, {history}) {
+        axios
+        .get("http://localhost/cart",{})
+        .then(function (res) {
+            let product_list = [...res.data];
+            dispatch(setCart(product_list))
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+    };
+};
+
+const deleteCartDB = (id) => {
+    return async function (dispatch, getState, {history}) {
+        axios
+        .delete("http://localhost/cart", id)
+        .then(function(res){
+            window.alert("삭제가 완료되었습니다.");
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+    }
+}
+
+export default handleActions(
+    {
+        [SET_CART]: (state, action) =>
+        produce(state, (draft) => {
+            draft.list = action.payload.product_list;
+        }),
+    }
+)
